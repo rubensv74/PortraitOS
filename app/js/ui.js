13dc4ede1d7cca78041e1a9d383f5026a1b88564
Rubén <seijoruben@gmail.com>
2026-07-19 00:48:58 +0200

    Implement PortraitUI module with UI functions

---
+++ app/js/ui.js
@@ -0,0 +1,65 @@
+const PortraitUI = (() => {
+
+    let elements = {};
+
+    function initialize(){}
+
+    function cacheElements(){}
+
+    function render(){}
+
+    function renderHeader(){}
+
+    function renderSidebar(){}
+
+    function renderWizard(){}
+
+    function renderStep(){}
+
+    function renderPhotos(){}
+
+    function renderIdentity(){}
+
+    function renderFaceLock(){}
+
+    function renderGoal(){}
+
+    function renderPerception(){}
+
+    function renderSummary(){}
+
+    function renderProgress(){}
+
+    function showToast(){}
+
+    function showLoading(){}
+
+    function hideLoading(){}
+
+    function openModal(){}
+
+    function closeModal(){}
+
+    function updateButtons(){}
+
+    return {
+
+        initialize,
+
+        render,
+
+        showToast,
+
+        showLoading,
+
+        hideLoading,
+
+        openModal,
+
+        closeModal
+
+    };
+
+})();
+
+window.PortraitUI = PortraitUI;
